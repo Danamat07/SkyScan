27c9a5ec95f6d34cc7ce98a9ced5a741920aad30
Matei_Dana <dana.matei@stud.ubbcluj.ro>
2025-03-05 15:51:17 +0200

    Set up enviroment

---
+++ skyScan.py
@@ -0,0 +1,5 @@
+import tkinter as tk            # for creating the GUI
+import requests                 # for fetching weather data from the OpenWeather API
+from tkinter import messagebox  # for showing pop-up error messages
+
+# 92b80276db8821606d5b387abf6575e1 - API KEY
